1abe0364fee2b56334012d55c84a53faccd68680
victoriaehrbar <victoriaehrbar@gmail.com>
2021-02-03 22:16:52 +0000

    Automatically backed up by Learn

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,12 @@
+# db/migrate/01_create_artists.rb
+ 
+class CreateArtists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+end
